4fccf26f2e9b26200fada5d6fe4bb91709755321
jbj0311 <bumjunjeong2@gmail.com>
2019-02-10 23:53:24 +0900

    youtube

---
+++ a.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+	int x;
+	x=5;
+	printf("%d", sizeof(x));
+	return 0;
+}
---
+++ b.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+	int x=50;
+	float y=123456789.123456789;
+	double z=123456789.123456789;
+	printf("x=%d\n", x);
+	printf("y=%d.2f\n", y);    
+	printf("z= %d.2f\n", z);
+	return 0;
+}
