4984d3207fc06d9c38f546a3c434d5c8196ccb79
jbj0311 <bumjunjeong2@gmail.com>
2019-02-13 23:12:18 +0900

    codeup

---
+++ 머성/codeup.kr/1020.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+	int i,j;
+	scanf("%06d-%07d", &i,&j);
+	printf("%06d%07d", i,j);
+	return 0;
+}
---
+++ 머성/codeup.kr/1022.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+	char  i[2001];
+	fgets(i,200,stdin);
+	printf("%s",i);
+	return 0;
+}
